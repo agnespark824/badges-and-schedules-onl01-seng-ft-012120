ab9cad37e40fb71c11a159693d5a139477455dd8
Agnes Park <agnespark824@gmail.com>
2020-01-22 18:26:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,13 @@
-def badge_maker(array)
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+  
+  
+  
+  
+  
+  
+  (array)
   array.each do |index|
     "Hello, my name is #{index}."
   end
